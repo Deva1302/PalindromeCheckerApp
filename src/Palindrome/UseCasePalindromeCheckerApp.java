8985e76e2b065bc952a96b97bc8200056323b48e
Devanshi Srivastava <devanshi.srivastava1302@gmail.com>
2026-02-19 09:13:48 +0530

    Base application setup

---
+++ src/Palindrome/UseCasePalindromeCheckerApp.java
@@ -0,0 +1,18 @@
+package Palindrome;
+
+public class UseCasePalindromeCheckerApp {
+    public static void main(String[] args) {
+        // ==============================
+        // UC1 - Application Entry & Welcome Message
+        // ==============================
+
+        System.out.println("====================================");
+        System.out.println("      PALINDROME CHECKER APP        ");
+        System.out.println("====================================");
+        System.out.println("Application Version: 1.0.0");
+        System.out.println("Developed for learning purposes.");
+        System.out.println("====================================");
+
+        // Program continues to next use case...
+    }
+}
