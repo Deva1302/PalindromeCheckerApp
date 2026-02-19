4213fe4f5439bc878578cf460662e81d7d0a57bd
Devanshi Srivastava <devanshi.srivastava1302@gmail.com>
2026-02-19 10:05:56 +0530

    UC6: Palindrome check using Queue and Stack

--- src/Palindrome/UseCasePalindromeCheckerApp.java
+++ src/Palindrome/UseCasePalindromeCheckerApp.java
@@ -1,13 +1,15 @@
 package Palindrome;
 
 import java.util.Stack;
+import java.util.Queue;
+import java.util.LinkedList;
 
 public class UseCasePalindromeCheckerApp {
 
     public static void main(String[] args) {
 
         // ==============================
-        // UC5 - Stack-Based Palindrome Checker
+        // UC6 - Queue + Stack Based Palindrome Check
         // ==============================
 
         System.out.println("====================================");
@@ -17,23 +19,32 @@ public class UseCasePalindromeCheckerApp {
         // Original string
         String original = "madam";
 
-        // Create a Stack
+        // Create Stack and Queue
         Stack<Character> stack = new Stack<>();
+        Queue<Character> queue = new LinkedList<>();
 
-        // Push all characters into stack
+        // Push and Enqueue characters
         for (int i = 0; i < original.length(); i++) {
-            stack.push(original.charAt(i));
+            char ch = original.charAt(i);
+            stack.push(ch);      // LIFO
+            queue.add(ch);       // FIFO
         }
 
-        // Pop characters and build reversed string
-        String reversed = "";
+        // Compare characters
+        boolean isPalindrome = true;
 
         while (!stack.isEmpty()) {
-            reversed = reversed + stack.pop();
+            char stackChar = stack.pop();    // Last in
+            char queueChar = queue.remove(); // First in
+
+            if (stackChar != queueChar) {
+                isPalindrome = false;
+                break;
+            }
         }
 
-        // Compare original and reversed
-        if (original.equals(reversed)) {
+        // Display result
+        if (isPalindrome) {
             System.out.println("The string \"" + original + "\" is a PALINDROME.");
         } else {
             System.out.println("The string \"" + original + "\" is NOT a palindrome.");
